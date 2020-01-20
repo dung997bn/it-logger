a257ee8762bec37cefa052ae4090e7b8d6d3cb21
DUNG <dung@gmail.com>
2020-01-21 00:01:22 +0700

    Lesson 75

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
-import LogItem from "./LogItem";
-import PreLoader from "../layout/PreLoader";
+import React, { useState, useEffect } from 'react';
+import LogItem from './LogItem';
+import PreLoader from '../layout/PreLoader';
 
 const Logs = () => {
   const [logs, setLogs] = useState([]);
@@ -8,11 +8,12 @@ const Logs = () => {
 
   useEffect(() => {
     getLogs();
+    // eslint-disable-next-line
   }, []);
 
   const getLogs = async () => {
     setLoading(true);
-    const res = await fetch("/logs");
+    const res = await fetch('/logs');
     const data = await res.json();
 
     setLogs(data);
@@ -20,15 +21,15 @@ const Logs = () => {
   };
 
   if (loading) {
-    return <PreLoader/>;
+    return <PreLoader />;
   }
   return (
-    <ul className="collection with-header">
-      <li className="collection-header">
-        <h4 className="center">System Logs</h4>
+    <ul className='collection with-header'>
+      <li className='collection-header'>
+        <h4 className='center'>System Logs</h4>
       </li>
       {!loading && logs.length === 0 ? (
-        <p className="center">No logs to show...</p>
+        <p className='center'>No logs to show...</p>
       ) : (
         logs.map(log => <LogItem key={log.id} log={log} />)
       )}
